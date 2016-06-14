ed48395758e7cf3056cb030e1ea1e6f9caff4aef
Arual728 <Arual728@users.noreply.github.com>
2016-06-13 20:00:46 -0500

    Delete conexion.php

--- conexion.php
+++
@@ -1,7 +0,0 @@
-<?php 
-// datos para la coneccion a mysql 
-$host="localhost";
-$usuario="root"; 
-$password="";
-$conectar=mysql_connect($host, $usuario, $password);
-mysql_select_db("db_blog",$conectar);
